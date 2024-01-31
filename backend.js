641420350d4fbee850316730b3eb1d4a622afceb
Shaik <shaikadam642@gmail.com>
2024-01-31 13:12:59 +0530

    31 JAN

--- backend.js
+++ backend.js
@@ -1,5 +1,10 @@
+const bodyParser = require("body-parser");
+const { exec } = require("child_process");
 const express = require("express");
 const app = express();
+const fs = require("fs");
+const cors = require("cors");
+const { stdin } = require("process");
 const port = 3000;
 
 
@@ -15,14 +20,37 @@ function generateRandomString(length) {
     
     return result;
 }
+app.use(cors())
+app.use(bodyParser.json());
+app.post('/code',async(req,res) => {
+    var x = generateRandomString(12);
+    // const cmd = `type nul > ${x}.cpp`;
+    // exec(cmd);
+    var tempcode = req.body.code
+    // console.log(req.body.code)
+   await fs.writeFile(`${x}.cpp`,(tempcode),(err)=>{
+        if(err)
+        {
+            res.status(401).send("Error in writing file");
+        }
+    })
+    const command = `g++ ${x}.cpp -o ${x} && ${x}.exe`;
+    exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          return;
+        }
+        if (stderr) {
+          console.error(`Error: ${stderr}`);
+          return;
+        }
+        if(stdin) {
+          
+          return;
+        }
+        console.log(`Output: ${stdout}`);
+      })
 
-
-app.post('/code',(req,res) => {
-  var x = generateRandomString(10);
-    const cmd = `${x}.cpp`;
-    exec(cmd)
-
-    
 })
 
 app.listen(port, () => {

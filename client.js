eabba0611526c735bc8d3e683b5910fa2706a086
Shaik <shaikadam642@gmail.com>
2024-02-09 13:00:57 +0530

    final touch

--- client.js
+++ client.js
@@ -2,15 +2,15 @@ function postcode() {
     const code = document.getElementById('code').value;
     const input = document.getElementById('input').value;
 
-    fetch('http://localhost:3000/code', {
+    fetch('https://codeswift-zeta.vercel.app/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ code, input }),
     })
-    .then(response => response.json())
-    .then(data => {
+    .then((resp) => resp.json())
+    .then((data) => {
         
         document.getElementById('output').innerText = data.stdout;
 

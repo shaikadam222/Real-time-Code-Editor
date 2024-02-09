5d44903cd4907d594e3a8780fcacfecdb218b233
Shaik <shaikadam642@gmail.com>
2024-02-10 00:25:36 +0530

    changes 10

--- client.js
+++ client.js
@@ -2,7 +2,7 @@ function postcode() {
     const code = document.getElementById('code').value;
     const input = document.getElementById('input').value;
 
-    fetch('https://imaginative-centaur-0345d7.netlify.app/code', {
+    fetch('https://app.netlify.com/sites/imaginative-centaur-0345d7', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',

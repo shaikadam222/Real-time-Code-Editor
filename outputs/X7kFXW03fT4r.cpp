d4f2574f23dba5da30cab699966082abbad37e44
Shaik <shaikadam642@gmail.com>
2024-02-09 13:14:52 +0530

    changes

--- outputs/X7kFXW03fT4r.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-   int x;
-   cout<<"Enter the value of x: "<<endl;
-   cin>>x;
-   cout<<"Entered value: "<<x<<endl;
-}
--- outputs/kFjfQepEcTBy.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-int main() {
-    int x;
-    std::cout << "Enter a value for x: ";
-    x=2;
-    std::cout << "You entered: " << x << std::endl;
-    return 0;
-}

57441405b1ff683e8bcae03fb33dbe4928fd9417
Shaik <shaikadam642@gmail.com>
2024-02-10 19:10:11 +0530

    alters 1

--- client.js
+++ client.js
@@ -13,7 +13,7 @@ function postcode() {
         body: JSON.stringify({ code, input }),
     })
         .then((resp) => {
-            resp.json().then((data) => {
+            resp.text().then((data) => {
                 document.getElementById('output').innerText = data.stdout;
                 console.log('Output from backend:', data.stdout);
                 console.log('Error from backend:', data.stderr);

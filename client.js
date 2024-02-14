d2b83a80e5896d32c995e21702829e3b062330c3
Shaik <shaikadam642@gmail.com>
2024-02-14 08:38:16 +0530

    verification

--- client.js
+++ client.js
@@ -2,20 +2,22 @@ function postcode() {
     const code = document.getElementById('code').value;
     const input = document.getElementById('input').value;
 
-    fetch('https://coders-iny9.onrender.com/', {
+    fetch('https://coders-iny9.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
         body: JSON.stringify({ code, input }),
     })
-    .then(response => response.json())
-    .then(data => {
-        // Display the output in the pre tag
-        document.getElementById('output').innerText = data.stdout;
-
-        console.log('Output from backend:', data.stdout);
-        console.log('Error from backend:', data.stderr);
+    .then((response) => {
+        console.log(response)
+        response.json().then(data => {
+            
+            document.getElementById('output').innerText = data.stdout;
+    
+            console.log('Output from backend:', data.stdout);
+            console.log('Error from backend:', data.stderr);
+        })
     })
     .catch(error => {
         console.error('Error:', error);
